f0aa0eb1f7e4fc4427f698e5faae6e08c2ff6ffe
steffenpedersen <steffen.pedersen@live.dk>
2022-04-05 14:06:29 +0200

    Add styling for detail page

--- src/pages/beerDetail/BeerDetailPage.tsx
+++ src/pages/beerDetail/BeerDetailPage.tsx
@@ -1,10 +1,46 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 import {
   getBeerAsync,
-  selectBeer
+  selectBeer,
 } from "../../components/beersList/beersSlice";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+
+  @media (min-width: ${(props) => props.theme.breakpoints.medium}) {
+    flex-direction: row;
+    gap: 100px;
+  }
+`;
+
+const Text = styled.p`
+  flex: 3;
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 350px;
+
+  background: ${(props) => props.theme.color.yellow};
+  padding: 35px;
+  border-radius: 8px;
+  box-shadow: 25px 25px 80px rgba(0, 0, 0, 0.07);
+
+  @media (min-width: ${(props) => props.theme.breakpoints.medium}) {
+    flex: 1;
+  }
+`;
+
+const Image = styled.img`
+  max-height: 100%;
+  width: auto;
+`;
 
 function BeerDetailPage() {
   const dispatch = useDispatch();
@@ -18,7 +54,13 @@ function BeerDetailPage() {
   return (
     <div>
       <h1>{beer.name}</h1>
-      <img src={beer.image_url} width={200} alt="" />
+      <h2 style={{ color: "#00000099" }}>{beer.tagline}</h2>
+      <Container>
+        <Text>{beer.description}</Text>
+        <ImageContainer>
+          <Image src={beer.image_url} alt={beer.name} />
+        </ImageContainer>
+      </Container>
     </div>
   );
 }
